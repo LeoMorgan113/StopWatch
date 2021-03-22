2b7b56d7ff74672a11644d085a447e41852dd877
LeoMorgan113 <mandragora1873@gmail.com>
2021-03-22 15:07:11 +0200

    StupWatch

--- src/App.js
+++ src/App.js
@@ -1,73 +1,76 @@
 import s from './App.module.css';
-import React, {useState, useEffect} from "react";
-import BtnComponent from "./components/BtnComponent";
-import Clock from "./components/Clock";
-import {delay} from "rxjs/operators";
+import React, {useEffect} from "react";
+import {fromEvent, interval} from "rxjs";
+import {debounceTime, buffer} from "rxjs/operators";
 
 
-function App() {
-    const [time, setTime] = useState({ms: 0, s: 0, m: 0, h: 0})
-    const [interv, setInterv] = useState();
-    const [status, setStatus] = useState(0);
-    // not started = 0, started = 1, stopped = 2
+const App = () => {
+    const myRef = React.createRef()
+    const start_stop = React.createRef()
 
-    const start = () => {
-        run();
-        setStatus(1);
-        setInterv(setInterval(run, 10));
-    }
-    const stop = () => {
-        clearInterval(interv);
-        setStatus(0);
-        setTime({
-            ms: 0,
-            s: 0,
-            m: 0,
-            h: 0
-        })
-    }
-    const wait = () => {
-        clearInterval(interv);
-        setStatus(2);
-    }
-    const reset = () => {
-        clearInterval(interv);
-        setStatus(0);
-        setTime({
-            ms: 0,
-            s: 0,
-            m: 0,
-            h: 0
-        })
-    }
+    const source = interval(100 /* ms */ );
 
-    const run = () => {
-        if(time.ms === 100){
-            time.s++;
-            time.ms = 0;
-        }
-        if(time.s === 60){
-            time.m++;
-            time.s = 0;
-        }
-        if(time.m === 60){
-            time.h++;
-            time.m = 0;
-        }
-        time.ms++;
-        return setTime({
-            ms: time.ms,
-            s: time.s,
-            m: time.m,
-            h: time.h
-        })
-    }
+    let started = false;
+    let time = 0;
+
+    useEffect(() =>{
+        const subscription$ = source.subscribe(
+            x => {
+                if(!started) return;
+                time++;
+                myRef.current.innerHTML = Math.floor(time/3600) +":" + Math.floor(time / 600) + ":" + Math.floor((time / 10) % 60) + ":" + (time % 10) + "0";
+            });
+        return () => subscription$.unsubscribe();
+    },[started])
+
+    useEffect(()=>{
+        const start$ = fromEvent(document.getElementById('start'), 'click')
+            .subscribe((e) => {
+                started = !started;
+                start_stop.current.innerText = 'Stop';
+                if(started === false){
+                    time = 0;
+                    myRef.current.innerHTML = "00:00:00:00";
+                    start_stop.current.innerText = 'Start'
+                }
+                console.log()
+            });
+        return () => started
+    },[started])
+
+    useEffect(() => {
+        const wait$ = fromEvent(document.getElementById('wait'), 'click');
+
+        const click$ = wait$
+            .pipe(buffer(wait$.pipe(debounceTime(250))))
+            .subscribe((e) => {
+                started = false;
+                console.log(started)
+
+            });
+    }, [started])
+    console.log(started)
+    useEffect(() => {
+        const reset$ = fromEvent(document.getElementById('reset'), 'click')
+            .subscribe((e) => {
+                started = false;
+                time = 0;
+                myRef.current.innerHTML = "00:00:00";
+            });
+    },[started])
+
+
     return (
         <div className={s.main_section}>
             <div className={s.clock_holder}>
+                <h1  ref={myRef} id="digital" className={s.clock_holder}>00:00:00:00</h1>
                 <div className={s.stopwatch}>
-                    <Clock time={time}/>
-                    <BtnComponent start={start} stop={stop} wait={wait} reset={reset} status={status}/>
+                     <button className={s.stopwatch_btn + " " + s.stopwatch_btn_gre}
+                             id="start" ref={start_stop}>Start</button>
+                     <button className={s.stopwatch_btn + " " + s.stopwatch_btn_red}
+                             id="wait">Wait</button>
+                     <button className={s.stopwatch_btn + " " + s.stopwatch_btn_yel}
+                             id="reset" >Reset</button>
                 </div>
             </div>
         </div>
